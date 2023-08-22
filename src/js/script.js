2499c732b322f143049015263a4624e2bba95a01
Débora Inocêncio <30512134+MQJogos@users.noreply.github.com>
2023-08-22 20:12:34 -0300

    Update script.js

--- src/js/script.js
+++ src/js/script.js
@@ -1,85 +1,49 @@
+$(document).ready(function(){
 
-const navbar = document.querySelector("[data-navbar]");
-const navbarLinks = document.querySelectorAll("[data-nav-link]");
-const menuToggleBtn = document.querySelector("[data-menu-toggle-btn]");
+    $('#menu').click(function(){
+        $(this).toggleClass('fa-times');
+        $('.navbar').toggleClass('nav-toggle');
+    });
 
-menuToggleBtn.addEventListener("click", function () {
-  navbar.classList.toggle("active");
-  this.classList.toggle("active");
-});
+    $(window).on('load scroll',function(){
 
-for (let i = 0; i < navbarLinks.length; i++) {
-  navbarLinks[i].addEventListener("click", function () {
-    navbar.classList.toggle("active");
-    menuToggleBtn.classList.toggle("active");
-  });
-}
+        $('#menu').removeClass('fa-times');
+        $('.navbar').removeClass('nav-toggle');
 
+        if($(window).scrollTop() > 60){
+            $('header .header-2').addClass('header-active');
+        }else{
+            $('header .header-2').removeClass('header-active');
+        }
 
+        $('section').each(function(){
 
-/**
- * header sticky & back to top
- */
+            let height = $(this).height();
+            let offset = $(this).offset().top - 200;
+            let top = $(window).scrollTop();
+            let id = $(this).attr('id');
 
-const header = document.querySelector("[data-header]");
-const backTopBtn = document.querySelector("[data-back-top-btn]");
+            if(top >= offset && top < offset + height){
+                $('.navbar ul li a').removeClass('active');
+                $('.navbar').find(`[href="#${id}"]`).addClass('active');
+            }
 
-window.addEventListener("scroll", function () {
-  if (window.scrollY >= 100) {
-    header.classList.add("active");
-    backTopBtn.classList.add("active");
-  } else {
-    header.classList.remove("active");
-    backTopBtn.classList.remove("active");
-  }
-});
+        });
 
+    });
 
+    $('.controls .buttons').click(function(){
 
-/**
- * search box toggle
- */
+        $(this).addClass('button-active').siblings().removeClass('button-active');
 
-const searchBtn = document.querySelector("[data-search-btn]");
-const searchContainer = document.querySelector("[data-search-container]");
-const searchSubmitBtn = document.querySelector("[data-search-submit-btn]");
-const searchCloseBtn = document.querySelector("[data-search-close-btn]");
+        let filter = $(this).attr('data-filter');
+        if(filter == 'all'){
+            $('.dish .image').show(400);
+        }else{
+            $('.dish .image').not('.'+filter).hide(200);
+            $('.dish .image').filter('.'+filter).show(400);
+        }
 
-const searchBoxElems = [searchBtn, searchSubmitBtn, searchCloseBtn];
-
-for (let i = 0; i < searchBoxElems.length; i++) {
-  searchBoxElems[i].addEventListener("click", function () {
-    searchContainer.classList.toggle("active");
-    document.body.classList.toggle("active");
-  });
-}
-
-
-
-/**
- * move cycle on scroll
- */
-
-const deliveryBoy = document.querySelector("[data-delivery-boy]");
-
-let deliveryBoyMove = -80;
-let lastScrollPos = 0;
-
-window.addEventListener("scroll", function () {
-
-  let deliveryBoyTopPos = deliveryBoy.getBoundingClientRect().top;
-
-  if (deliveryBoyTopPos < 500 && deliveryBoyTopPos > -250) {
-    let activeScrollPos = window.scrollY;
-
-    if (lastScrollPos < activeScrollPos) {
-      deliveryBoyMove += 1;
-    } else {
-      deliveryBoyMove -= 1;
-    }
-
-    lastScrollPos = activeScrollPos;
-    deliveryBoy.style.transform = `translateX(${deliveryBoyMove}px)`;
-  }
+    });
 
 });
