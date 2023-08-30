465f2d6b791062da9a5a2e9c4dab03aad1768ff8
Débora Inocêncio <30512134+MQJogos@users.noreply.github.com>
2023-08-29 21:47:07 -0300

    Update script.js

--- src/js/script.js
+++ src/js/script.js
@@ -5,7 +5,9 @@ let navbar = document.querySelector('.navbar');
 
 window.onscroll = () =>{
     searchBtn.classList.remove('fa-times');
-    searchBar.classList.remove('active');   
+    searchBar.classList.remove('active');  
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
 }
 
 menu.addEventListener('click', () =>{
